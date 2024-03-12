22dbe111bd1a2271c8b16fd0de8052206c35682c
caua <aquilesgamer@gmail.com>
2024-03-12 17:08:46 -0300

    criando livros

--- Index.ts
+++ Index.ts
@@ -12,8 +12,13 @@ const usuario1 = new Usuario("Caua", "012.164.379-31", [], [], [], 0)
 const usuario2 = new Usuario("andre", "012.111.000-31", [], [], [], 0)
 console.log(usuario1, usuario2);
 
-const livro1 = new Livros("Livro1", "autor1", GeneroLivro.DRAMA, StatusLivro.DISPONIVEL)
-console.log(livro1)
+const livro1 = new Livros("Livro 1", "autor 1", GeneroLivro.DRAMA, StatusLivro.DISPONIVEL)
+const livro2 = new Livros("Livro 2", "autor 2", GeneroLivro.ROMANCE, StatusLivro.DISPONIVEL)
+const livro3 = new Livros("Livrov3", "autor 3", GeneroLivro.FICCAO, StatusLivro.DISPONIVEL)
+const livro4 = new Livros("Livro 4", "autor 4", GeneroLivro.FANTASIA, StatusLivro.ATRASADO)
+const livro5 = new Livros("Livro 5", "autor 5", GeneroLivro.DRAMA, StatusLivro.EMPRESTADO)
+const livro6 = new Livros("Livro 6", "autor 6", GeneroLivro.ROMANCE, StatusLivro.RESERVADO)
+console.log(livro1, livro2, livro3, livro4, livro5, livro6)
 
 
 
--- index.js
+++ index.js
@@ -11,8 +11,13 @@ var Usuario_1 = require("./Usuario");
 var usuario1 = new Usuario_1.Usuario("Caua", "012.164.379-31", [], [], [], 0);
 var usuario2 = new Usuario_1.Usuario("andre", "012.111.000-31", [], [], [], 0);
 console.log(usuario1, usuario2);
-var livro1 = new Livros_1.Livros("Livro1", "autor1", GeneroLivro_1.GeneroLivro.DRAMA, StatusLivro_1.StatusLivro.DISPONIVEL);
-console.log(livro1);
+var livro1 = new Livros_1.Livros("Livro 1", "autor 1", GeneroLivro_1.GeneroLivro.DRAMA, StatusLivro_1.StatusLivro.DISPONIVEL);
+var livro2 = new Livros_1.Livros("Livro 2", "autor 2", GeneroLivro_1.GeneroLivro.ROMANCE, StatusLivro_1.StatusLivro.DISPONIVEL);
+var livro3 = new Livros_1.Livros("Livrov3", "autor 3", GeneroLivro_1.GeneroLivro.FICCAO, StatusLivro_1.StatusLivro.DISPONIVEL);
+var livro4 = new Livros_1.Livros("Livro 4", "autor 4", GeneroLivro_1.GeneroLivro.FANTASIA, StatusLivro_1.StatusLivro.ATRASADO);
+var livro5 = new Livros_1.Livros("Livro 5", "autor 5", GeneroLivro_1.GeneroLivro.DRAMA, StatusLivro_1.StatusLivro.EMPRESTADO);
+var livro6 = new Livros_1.Livros("Livro 6", "autor 6", GeneroLivro_1.GeneroLivro.ROMANCE, StatusLivro_1.StatusLivro.RESERVADO);
+console.log(livro1, livro2, livro3, livro4, livro5, livro6);
 // Cria biblioteca
 // const biblioteca = { livro1, livro2, livro3 }
 // Exibe toda biblioteca
